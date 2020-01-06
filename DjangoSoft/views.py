b7cdcc4a4e809de69b4430c1c0bd64cd2cfe0cb6
pythonuser <djangopython143@gmail.com>
2020-01-06 09:02:03 -0800

    version1

---
+++ DjangoSoft/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .models import Course,FeedbackData,ContactData
+
+from .forms import FeedbackForm
+
+
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return render(request,'home.html')
+def contact(request):
+    if request.method=='POST':
+        data=ContactData(name=request.POST.get('name'),
+                    email=request.POST.get('email_id'),
+                    mobile=request.POST.get('mobile'),
+                    comments=request.POST.get('message'),
+                    date=date1)
+        data.save()
+        return render(request,"callyou.html")
+    else:
+        return render(request,'contact.html')
+def services(request):
+    data=Course.objects.all()
+    return render(request,'services.html',{'data':data})
+from datetime import datetime
+date1=datetime.now()
+def feedback_view(request):
+    if request.method=='POST':
+        fform=FeedbackForm(request.POST)
+        if fform.is_valid():
+            name1=request.POST.get('Name')
+            rating1=request.POST.get('Rating')
+            feedback1=request.POST.get('Feedback')
+            data=FeedbackData(name=name1,rating=rating1,feedback=feedback1,date=date1)
+            data.save()
+            fform=FeedbackForm()
+            Feedbacks = FeedbackData.objects.all()
+            return render(request,'feedback.html',{'fform':fform,'Feedbacks':Feedbacks})
+        else:
+            return HttpResponse('Invalid Form')
+    else:
+        fform=FeedbackForm()
+        Feedbacks=FeedbackData.objects.all()
+        return render(request,'feedback.html',{'fform':fform,'Feedbacks':Feedbacks})
+
+
+def gallery(request):
+    return render(request,'gallery.html')
